aa78c7d25df710a859347d89b94dca42db29eb84
Talha Yusif <talhaysif06@gmail.com>
2025-11-11 20:53:50 +0000

    Corrected Multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@
 number=int(input("Enter a number to see its multiplication table:"))
 for x in range(number,number+1):
     for y in range(1,11):
-        print(f"{x} * {y} is {x*y}")
+        print(f"{x} * {y} = {x*y}")
