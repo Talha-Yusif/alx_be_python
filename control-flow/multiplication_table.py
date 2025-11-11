82cb0023a8586ecc277017c959dc8509cff22601
Talha Yusif <talhaysif06@gmail.com>
2025-11-11 21:11:34 +0000

    resolved issue in multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,4 @@
 
 number=int(input("Enter a number to see its multiplication table:"))
-for x in range(number,number+1):
-    for y in range(1,11):
-        print(f"{x} * {y} = {x*y}")
+for y in range(1,11):
+    print(f"{number} * {y} = {number*y}")
