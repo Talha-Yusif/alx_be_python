8e637ee13727147c855f3ddbf148aee4673d27e4
Talha Yusif <talhaysif06@gmail.com>
2025-11-28 15:53:56 +0000

    Test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,33 +7,33 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def test_additin(self):
-        self.assertEqual(calc.add(10,2), 12)
-        self.assertEqual(calc.add(-10,-2), -12)
-        self.assertEqual(calc.add(-10,2), -8)
-        self.assertEqual(calc.add(0,0), 0)
+        self.assertEqual(self.calc.add(10,2), 12)
+        self.assertEqual(self.calc.add(-10,-2), -12)
+        self.assertEqual(self.calc.add(-10,2), -8)
+        self.assertEqual(self.calc.add(0,0), 0)
     
     def test_subtraction(self):
-        self.assertEqual(calc.subtract(10,2), 8)
-        self.assertEqual(calc.subtract(-10,-2), -8)
-        self.assertEqual(calc.subtract(10,-2), 12)
-        self.assertEqual(calc.subtract(0,0), 0)
+        self.assertEqual(self.calc.subtract(10,2), 8)
+        self.assertEqual(self.calc.subtract(-10,-2), -8)
+        self.assertEqual(self.calc.subtract(10,-2), 12)
+        self.assertEqual(self.calc.subtract(0,0), 0)
 
     def test_multiply(self):
-        self.assertEqual(calc.multiply(10,2), 20)
-        self.assertEqual(calc.multiply(-10,-2), 20)
-        self.assertEqual(calc.multiply(10,-2), -20)
-        self.assertEqual(calc.multiply(-10,2), -20)
-        self.assertEqual(calc.multiply(0,0), 0)
+        self.assertEqual(self.calc.multiply(10,2), 20)
+        self.assertEqual(self.calc.multiply(-10,-2), 20)
+        self.assertEqual(self.calc.multiply(10,-2), -20)
+        self.assertEqual(self.calc.multiply(-10,2), -20)
+        self.assertEqual(self.calc.multiply(0,0), 0)
 
     def test_divide(self):
-        self.assertEqual(calc.divide(10,2), 5)
-        self.assertEqual(calc.divide(-10,2), -5)
-        self.assertEqual(calc.divide(10,-2), -5)
-        self.assertEqual(calc.divide(16,5),3.2 )
+        self.assertEqual(self.calc.divide(10,2), 5)
+        self.assertEqual(self.calc.divide(-10,2), -5)
+        self.assertEqual(self.calc.divide(10,-2), -5)
+        self.assertEqual(self.calc.divide(16,5),3.2 )
 
         with self.assertRaises(ZeroDivisionError):
 
-            calc.divide(10,0)
+            self.calc.divide(10,0)
 
 if __name__=="__main":
     unittest.main()
