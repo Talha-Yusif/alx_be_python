71b835a9f6abcfe7136bec320369b60e4ed0ff36
Talha Yusif <talhaysif06@gmail.com>
2025-11-28 15:34:39 +0000

    unittest

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,38 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class Rich(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(10,2), 12)
+        self.assertEqual(add(-10,-2), -12)
+        self.assertEqual(add(-10,2), -8)
+        self.assertEqual(add(0,0), 0)
+    
+    def test_subtract(self):
+        self.assertEqual(subtract(10,2), 8)
+        self.assertEqual(subtract(-10,-2), -8)
+        self.assertEqual(subtract(10,-2), 12)
+        self.assertEqual(subtract(0,0), 0)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(10,2), 20)
+        self.assertEqual(multiply(-10,-2), 20)
+        self.assertEqual(multiply(10,-2), -20)
+        self.assertEqual(multiply(-10,2), -20)
+        self.assertEqual(multiply(0,0), 0)
+
+    def test_divide(self):
+        self.assertEqual(divide(10,2), 5)
+        self.assertEqual(divide(-10,2), -5)
+        self.assertEqual(divide(10,-2), -5)
+        self.assertEqual(divide(16,5),3.2 )
+
+        with self.assertRaises(ZeroDivisionError):
+
+            divide(10,0)
+
+if __name__=="__main":
+    unittest.main()
+
+
