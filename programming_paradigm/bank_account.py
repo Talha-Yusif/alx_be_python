8bd7ebd8c783a45bdc0d0e32373b83f7572a98d2
Talha Yusif <talhaysif06@gmail.com>
2025-11-28 11:14:51 +0000

    BankApp

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+
+class BankAccount:
+    def __init__(self,account_balance=0):
+        self.__account_balance=account_balance
+    def deposit(self,amount):
+        
+        self.__account_balance+=amount
+    def withdraw(self,amount):
+        if 0<amount<=self.__account_balance:
+            self.__account_balance-=amount
+            return True
+        else:
+            
+            return False
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance}")
+
+
+
+        
