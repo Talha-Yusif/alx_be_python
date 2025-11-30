18271e9caa2849b3edd664d443ad2f39d667caee
Talha Yusif <talhaysif06@gmail.com>
2025-11-30 13:01:20 +0000

    Library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,47 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title                 
+        self.author = author               
+        self._is_checked_out = False       
+
+    def check_out(self):
+        if self._is_checked_out == False:
+            self._is_checked_out = True
+
+    def return_book(self):
+        if self._is_checked_out == True:
+            self._is_checked_out = False
+
+    def is_available(self):
+        if self._is_checked_out == False:
+            return True
+        else:
+            return False
+
+
+
+class Library:
+    def __init__(self):
+        self._books = []     
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                book.check_out()
+                return
+        print(f"Book '{title}' not found.")
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                book.return_book()
+                return
+        print(f"Book '{title}' not found.")
+
+    def list_available_books(self):
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
